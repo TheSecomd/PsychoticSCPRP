42b31abd5618ca65013dba83fe97151ee9cf2d97
TheSecomd <realkianwalls@gmail.com>
2020-07-24 09:47:22 -0700

    Update config.js

--- sleekLoad2/js/config.js
+++ sleekLoad2/js/config.js
@@ -122,7 +122,7 @@ var l_musicDisplay = true;
 // (Place the ogg files in the music folder)
 // (You can also use external URL for the ogg files)
 var l_musicPlaylist = [
-	{ogg: "https://www.youtube.com/watch?v=DOAvSbUmXAA", name: "SCP Background Ambient"},
+	{URL: "https://www.youtube.com/watch?v=DOAvSbUmXAA", name: "SCP Background Ambient"},
 ];
 
 // Random music order?
